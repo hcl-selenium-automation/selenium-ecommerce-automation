576fd811f0144df990750c830c9ec5b681cbda35
Rajneesh Kushwaha <160695415+Rajneesh-1234@users.noreply.github.com>
2026-02-22 10:01:14 +0530

    My local changes

--- src/main/java/reports/ExtentManager.java
+++ src/main/java/reports/ExtentManager.java
@@ -1,11 +1,11 @@
 package reports;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
-import org.testng.*;
-
-import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
